79080159020a4a0721344e3649e0b584bce0aa2e
seonghee yu <zhdzhdgl@gmail.com>
2019-06-11 17:48:26 +0900

    타이타닉

---
+++ java/com/amg/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.amg.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * HomeController
+ */
+
+@Controller // BEAN으로 등록 함.
+ public class HomeController {
+    
+    @RequestMapping("/") //어떤 url과 매핑하는가 "/"는 root url Bean은 빈인데 매핑이라 함. "/"이게 들어오면 밑에껄 하라. "Bean으로 등록되려면 class만 가능"
+    public String index() {
+        System.out.println("루트 url 경로로 들어옴");
+        return "index";
+    }
+    
+}
---
+++ resources/static/resources/js/index.js
@@ -0,0 +1,27 @@
+alert('자바스크립트 연결 성공!!');
+
+// wrapper은 객체/. index.html 전체는 하나의 Dom 나머지는 컴포넌트로 처리할것임. <div id="wrapper"> <- 컴포넌트임.
+
+
+var wrapper = document.querySelector("#wrapper");
+wrapper.innerHTML = '<h1>SPA 시작</h1>'
++'<div id="target">삭제할 내용</div>'
++'<button id="btn">지워버리자</button>'
++'<button id="btn2">추가하자</button>';
+
+// 아래는 객체임. 컴포넌트.
+var btn = document.querySelector('#btn');
+var target = document.getElementById('target');
+
+btn.addEventListener('click', function(){
+    target.innerHTML = '';
+});
+
+var btn2 = document.querySelector('#btn2');
+
+btn2.addEventListener('click', function() {
+    var temp = document.creatTextNode('새로 추가 됨');
+    target.appendChild(temp);
+});
+
+
